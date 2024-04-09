93e46a1437412ecd411e76bc53185b8627568154
MONG-SIL <76767087+MONG-SIL@users.noreply.github.com>
2024-04-09 11:49:02 +0900

    Add files via upload
    실습 커밋

---
+++ modal_p/index.js
@@ -0,0 +1,15 @@
+
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modal = document.getElementById("modal-wrapper")
+
+
+function openM(){
+    console.log(modal)
+    modal.style.display = "flex"
+}
+
+function closeM(){
+    modal.style.display = "none"
+}
+
